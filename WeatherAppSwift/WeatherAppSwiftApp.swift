aa1f56b023ebfa5e8b5a13ba938d495aad8fe451
namandhiman <namandhiman98@gmail.com>
2024-11-21 22:54:16 +0530

    Initial Commit

---
+++ WeatherAppSwift/WeatherAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftApp.swift
+//  WeatherAppSwift
+//
+//  Created by Naman Dhiman on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
